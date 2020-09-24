aad1de09dd3874ca05aded93cbb804f88cfceda6
Responic <liyulong950908@163.com>
2020-09-24 19:47:44 +0800

    I add a new R file into the folder.This R script is used to study

---
+++ 20200917E1.R
@@ -0,0 +1,8 @@
+Keeling_Data <- read.csv(file = "co2_mm_mlo.csv", header = T)
+Keeling_Data[2]
+Keeling_Data[[2]]
+Keeling_Data$month
+Keeling_Data["month"]
+Keeling_Data[2, 2]
+Keeling_Data[, 2]
+Keeling_Data[2, ]
